f84cc1bbe4ad3324c52ccfb72b3149be0cb2b4a4
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2021-11-11 20:00:53 -0300

    Update 20:00 11-11

--- eggplant-brownie/eggplant-brownie/MyPlayground.playground/Pages/Classe-item-arrays em classes de trabalho-11-11-21.xcplaygroundpage/Contents.swift
+++ eggplant-brownie/eggplant-brownie/MyPlayground.playground/Pages/Classe-item-arrays em classes de trabalho-11-11-21.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
 //: [Previous](@previous)
 
 import Foundation
+import UIKit
+class Refeicao {
+ 
+    var nome: String
+    var felicidade: String
+    var itens: Array<Item> = []
+    
+    init(nome: String, felicidade: String){
+        self.nome = nome
+        self.felicidade = felicidade
+        print(nome)
+    }
+}
 
-var greeting = "Hello, playground"
+class Item {
+    var nome: String
+    var calorias: Double
+   
+    
+    init(nome: String, calorias: Double){
+        self.nome = nome
+        self.calorias = calorias
+        print(nome)
+    }
+    
+}
+
+let arroz = Item(nome: "Arroz", calorias: 52.0)
+let feijao = Item(nome: "Feijao", calorias: 66.9)
+let carne = Item(nome: "Contra-Filé", calorias: 200.01)
 
 //: [Next](@next)
