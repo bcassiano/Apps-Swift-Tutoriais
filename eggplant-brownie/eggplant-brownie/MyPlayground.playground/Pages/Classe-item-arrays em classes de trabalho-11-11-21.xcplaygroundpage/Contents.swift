10a2fb1104ca8b4dd69a1dd3825057017e6cdda2
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2021-11-12 23:55:34 -0300

    Finalizando classes e append

--- eggplant-brownie/eggplant-brownie/MyPlayground.playground/Pages/Classe-item-arrays em classes de trabalho-11-11-21.xcplaygroundpage/Contents.swift
+++ eggplant-brownie/eggplant-brownie/MyPlayground.playground/Pages/Classe-item-arrays em classes de trabalho-11-11-21.xcplaygroundpage/Contents.swift
@@ -4,17 +4,33 @@ import Foundation
 import UIKit
 class Refeicao {
  
+    // MARK: - Atributos
+    
     var nome: String
     var felicidade: String
     var itens: Array<Item> = []
     
+    
+    // MARK: - Construtor
+    
     init(nome: String, felicidade: String){
         self.nome = nome
         self.felicidade = felicidade
-        print(nome)
+        
     }
-}
 
+// MARK: - Metodos
+
+func totalDeCalorias() -> Double {
+    var total = 0.0
+    
+    for item in itens {
+        total += item.calorias
+        }
+    return total
+    }
+    
+}
 class Item {
     var nome: String
     var calorias: Double
@@ -26,10 +42,23 @@ class Item {
         print(nome)
     }
     
+    
 }
 
 let arroz = Item(nome: "Arroz", calorias: 52.0)
 let feijao = Item(nome: "Feijao", calorias: 66.9)
 let carne = Item(nome: "Contra-Filé", calorias: 200.01)
+
+let refeicao = Refeicao(nome: "Almoço", felicidade: "5")
+refeicao.itens.append(arroz)
+refeicao.itens.append(feijao)
+refeicao.itens.append(carne)
+
+print(refeicao.nome)
+if let primeiroItemDaLista = refeicao.itens.first{
+    print(primeiroItemDaLista.nome)
+}
+
+print (refeicao.totalDeCalorias())
 
 //: [Next](@next)
