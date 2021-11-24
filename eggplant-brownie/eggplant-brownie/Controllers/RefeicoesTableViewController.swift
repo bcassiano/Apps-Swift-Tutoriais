bf634ed0dfcae8b0f6c9d4de289ee4ae0c69302b
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2021-11-24 08:00:31 -0300

    Adicionando melhor Nomenclatura
    Alterando a nomenclatura do delegate para melhor compreensão de suas funções no app

--- eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RefeicoesTableViewController: UITableViewController, ViewControllerDelegate {
+class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDelegate {
     
     var refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
                      Refeicao(nome: "Pizza", felicidade: 4),
