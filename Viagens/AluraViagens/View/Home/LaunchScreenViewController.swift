87a0ce947c973a083c23386c9c86d48fedd56122
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2022-05-06 05:26:27 -0300

    Adicionando animações na tela Launch-Screen
    Alterando para View e gerenciando a tela de navegação Root

---
+++ Viagens/AluraViagens/View/Home/LaunchScreenViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LaunchScreenViewController.swift
+//  AluraViagens
+//
+//  Created by Bruno Cassiano on 06/05/22.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var constraintTituloTop: NSLayoutConstraint!
+    
+    
+    // MARK: - View life cycle
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        iniciaAnimacao()
+    }
+    // MARK: - Metodos
+    
+    func iniciaAnimacao() {
+        constraintTituloTop.constant = 300
+        UIView.animate(withDuration: 1.0, animations: {
+            self.view.layoutIfNeeded()
+        }) { (_ ) in
+         // TO DO: -
+            self.irParaHome()
+        }
+    }
+
+    func irParaHome() {
+        let tabBar = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "tab-bar")
+        
+        present(tabBar,animated: true, completion: nil)
+    }
+    
+}
