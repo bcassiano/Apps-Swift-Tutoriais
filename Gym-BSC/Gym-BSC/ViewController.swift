c15bd804302af013287d3adbc71a56f14b05cb43
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2022-05-17 15:07:14 -0300

    Inclusao de ViewController
    Adicionando View

--- Gym-BSC/Gym-BSC/ViewController.swift
+++ Gym-BSC/Gym-BSC/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Exercicíos \(indexPath.row)"
+        
+        return cell
+    }
 }
+
 
